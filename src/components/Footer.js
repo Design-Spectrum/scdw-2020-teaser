59167c2c55c2771d05d3448a1d475f26b7c3d6a7
jinbread <jinjae.lee@icloud.com>
2020-09-30 21:58:03 +0900

    css minor fix 3

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,9 @@ export function Footer() {
   return (
     <div className="footer">
       <div className="container-footer">
-        <a href="https://designspectrum.org"><img className="spectrum-logo" src={spectrumLogo} alt="Design Spectrum" /></a>
-        <a href="https://www.instagram.com/designspectrum_official/"><img className="insta-logo" src={instaLogo} alt="Design Spectrum Instagram link" /></a>
-        <a href="https://www.facebook.com/sharedesignspectrum"><img className="fb-logo" src={fbLogo} alt="Design Spectrum Facebook link" /></a>
+        <a className="spectrum-logo" href="https://designspectrum.org"><img className="spectrum-logo" src={spectrumLogo} alt="Design Spectrum" /></a>
+        <a className="insta-logo" href="https://www.instagram.com/designspectrum_official/"><img className="insta-logo" src={instaLogo} alt="Design Spectrum Instagram link" /></a>
+        <a className="fb-logo" href="https://www.facebook.com/sharedesignspectrum"><img className="fb-logo" src={fbLogo} alt="Design Spectrum Facebook link" /></a>
       </div>
     </div>
   );
