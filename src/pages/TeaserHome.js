f5291f8e27d7e4a8c9e296973d557515aad087ae
jinbread <jinjae.lee@icloud.com>
2020-10-20 22:52:13 +0900

    style updated

--- src/pages/TeaserHome.js
+++ src/pages/TeaserHome.js
@@ -13,9 +13,11 @@ export function TeaserHome() {
     stiffness: 100,
   };
 
+  const size = useWindowSize();
+
   return (
     <>
-      <div className="video-container">
+      <div className={size.height >= 700 ? "video-container" : "video-container-xs"}>
         <video className="videoTag" autoPlay playsInline loop muted>
           <source src={backgroundVideo} type="video/mp4" />
         </video>
@@ -43,8 +45,8 @@ export function TeaserHome() {
               <AnimatedText delay={0.9} yPos={0}>
                 <div className="body-1">
                   코로나19로 열린 비대면 시대. <br/>2020 스펙트럼콘은 새로운 형식의
-                  온라인 콘퍼런스를 시도합니다. <br/>이번 콘퍼런스는 온라인으로, 2주
-                  동안 네 가지 주제로 진행합니다. <br/>퀄리티 높은 온라인 경험을
+                  온라인 콘퍼런스를 시도합니다. {size.width >= 640 && <br/>}이번 콘퍼런스는 온라인으로, 2주
+                  동안 네 가지 주제로 진행합니다. {size.width >= 640 && <br/>}퀄리티 높은 온라인 경험을
                   드리기 위해, 스타트업 미디어 채널 EO와 협업합니다.
                 </div>
               </AnimatedText>
@@ -56,8 +58,8 @@ export function TeaserHome() {
               <AnimatedText delay={0.9} yPos={0}>
                 <div className="body-1">
                   프로덕트 제작 과정에 참여하는 모든 이들과 함께합니다.
-                  <br/>디자이너
-                  뿐만 아니라, 그들과 협업하는 팀원들도 스피커로 초청하여 <br/>팀
+                  {size.width >= 640 && <br/>}디자이너
+                  뿐만 아니라, 그들과 협업하는 팀원들도 스피커로 초청하여 {size.width >= 640 && <br/>}팀
                   단위의 경험을 공유합니다.
                 </div>
               </AnimatedText>
@@ -69,9 +71,9 @@ export function TeaserHome() {
               <AnimatedText delay={0.9} yPos={0}>
                 <div className="body-1">
                   번듯한 성공이 아닌 솔직한 실패를 공유합니다.
-                  <br />
+                  {size.width >= 640 && <br/>}
                   새로운 시대를 대처하는 저마다의 방법들을 결과만이 아닌
-                  <br />
+                  {size.width >= 640 && <br/>}
                   과정에도 초점을 맞추어 전달합니다.
                 </div>
               </AnimatedText>
@@ -82,4 +84,36 @@ export function TeaserHome() {
       </div>
     </>
   );
+}
+
+
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = React.useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  React.useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+
+  return windowSize;
 }
